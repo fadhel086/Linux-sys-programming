98f8f9c0d4950782b39fb833e0f54edc0df7b67e
Fadhel habeeb <fadhel086@gmail.com>
2020-04-22 21:33:59 +0530

    1. Moved library files to lib/ 2. Added functionality to print DS content using call_backs

---
+++ Doubly_Linked_List_lib/lib/dll.h
@@ -0,0 +1,46 @@
+#ifndef _DLL_H_
+#define _DLL_H_
+
+#include <stdio.h>
+
+typedef struct dll_node_ {
+	void *data;
+	struct dll_node_ *left, *right;
+} dll_node_t;
+
+typedef struct dll_ {
+	dll_node_t *head;
+	void (*print_node)(void *node);
+}dll_t;
+
+/* Initialize the doubly linked list head
+   returns head
+*/
+dll_t *init_dll();
+
+/* register print node callbacl */
+void register_print_node_callback(dll_t *dll, void (*call_back)(void *));
+
+/* Procedure to dump the database contents */
+void dump_db(dll_t *);
+
+/* Inserts data in a sorted manner,
+   Returns 0 on success, and -1 on failure
+ */
+
+int dll_priority_insert_data(dll_t *dll, void *data);
+/* Add's data to the doubly linked list
+   returns 0 on success, -1 on error
+*/
+int add_data(dll_t *dll, void *app_data);
+
+/* return 0 on success and -1 on failure */
+int remove_data_from_dll_by_data_ptr(dll_t *dll, void *app_data);
+
+/* return 0 if empty, -1 if not empty */
+int is_dll_empty(dll_t *dll);
+
+/* delete all nodes from dll, but do not free appln data */
+void drain_dll(dll_t *dll);
+
+#endif
---
+++ Doubly_Linked_List_lib/lib/dll_util.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include "dll.h"
+
+int remove_data_from_dll_by_data_ptr(dll_t *dll, void *app_data)
+{
+	dll_node_t *remove_node = NULL;
+
+	if (!dll || !dll->head || !app_data)
+		return -1;
+
+	remove_node = dll->head;
+	// if data is in head_node, remove and return
+	if (remove_node->data == app_data) {
+		dll->head = dll->head->right;
+		dll->head->left = NULL;
+		free(remove_node);
+		return 0;
+	}
+	remove_node = remove_node->right;
+	
+	while(remove_node)
+	{
+		if (remove_node->data == app_data) {
+			remove_node->left->right = remove_node->right;
+			remove_node->right->left = remove_node->left;
+			free(remove_node);
+			return 0;
+		}		
+		remove_node = remove_node->right;
+	}	
+	return -1;
+}
+
+int is_dll_empty(dll_t *dll)
+{
+	if (!dll || !dll->head)
+		return 0;
+	return -1;
+}
+
+void drain_dll(dll_t *dll)
+{
+	dll_node_t *drain_node = NULL;
+
+ 	if (!dll || !dll->head)
+		return;
+	
+	drain_node = dll->head;
+	while(drain_node)
+	{	
+		// Move head to next node and remove present node
+		dll->head = drain_node->right;
+		free(drain_node);
+		drain_node = dll->head;		
+	}			
+}
